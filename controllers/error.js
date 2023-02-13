04874e0e5dbfc967f6393a441e7e19975bd5d1e5
Zuber8960 <zuberahmad8960@gmail.com>
2023-02-13 10:24:02 +0530

    !!

--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
 exports.get404 = (req, res, next) => {
-    console.log('===========================================================================');
     res.status(404).sendFile(path.join(__dirname,'../','frontend/html/error.html'));
 }
--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
 exports.home = (req, res, next) => {
-    console.log('===========================================================================');
     res.status(202).sendFile(path.join(__dirname,'../','frontend/html/home.html'));
 }
